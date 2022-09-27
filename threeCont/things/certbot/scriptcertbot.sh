34fdcd4739d234ce5902fa744217f0ca500d0772
oURMIo <dimach.98.ru@gmail.com>
2022-09-27 19:15:34 +0300

    trr

--- threeCont/things/certbot/scriptcertbot.sh
+++ threeCont/things/certbot/scriptcertbot.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=chserv.ddns.net
+domains=(chserv.ddns.net)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="urmi.98.ru@gmail.com" # Adding a valid address is strongly recommended
